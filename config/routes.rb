659346a97397cfd9e9912706a9833cc934c2846d
Yemi Arole <yarole7@gmail.com>
2023-01-28 16:41:43 +0000

    creating routes and controller and little bit of the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'bookmarks/destroy'
+  # get 'list/index'
+  # get 'list/show'
+  # get 'list/new'
+  # get 'list/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "lists#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
